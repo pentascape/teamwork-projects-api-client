cea16df50ee97e920742c359a5827cf6d308df4f
Alex Powell <alexjh.powell@gmail.com>
2019-05-21 10:23:39 +0100

    Fixing task list creation options.

--- types/tasklists.d.ts
+++ types/tasklists.d.ts
@@ -17,65 +17,65 @@ export namespace TaskLists {
   }
 
 
-  export interface TaskListsCreateRequestOptions {
-    projectId: number;
-    name: string;
-    private: boolean;
-    pinned: boolean;
-    "milestone-id": string;
-    description: string;
-    "todo-list-template-id": string;
+  interface TaskListsCreateUpdateSharedRequestOptions {
+    name?: string;
+    "milestone-id"?: string;
+    description?: string;
+    private?: boolean;
+    "grant-access-to"?: string;
   }
 
 
-  export interface TaskListsUpdateRequestOptions {
-    id: string;
+  export interface TaskListsCreateRequestOptions extends TaskListsCreateUpdateSharedRequestOptions {
+    projectId: number;
     name: string;
+    pinned?: boolean;
+  }
+
+
+  export interface TaskListsUpdateRequestOptions extends TaskListsCreateUpdateSharedRequestOptions {
+    id: string;
+  }
+
+
+  interface TaskListsResponseItemShared {
+    name: string;
+    pinned: boolean;
     "milestone-id": string;
     description: string;
-    private: string;
-    "grant-access-to": string;
+    "uncompleted-count": number;
+    id: string;
+    complete: boolean;
+    private: boolean;
+    position: number;
+  }
+
+
+  interface TaskListsListResponseItem extends TaskListsResponseItemShared {
+    isTemplate: boolean;
+    status: string;
+    projectId: string;
+    projectName: string;
+    DLM: null;
+  }
+
+
+  interface TaskListsFetchResponseItem extends TaskListsResponseItemShared {
+    projectid: string;
+    "overdue-count": string;
+    "project-name": string;
+    pinned: boolean;
+    "completed-count": string;
   }
 
 
   export interface TaskListsListResponse extends StatusResponse {
-    tasklists: [
-      {
-        name: string;
-        pinned: boolean;
-        "milestone-id": string;
-        description: string;
-        "uncompleted-count": number;
-        id: string;
-        complete: boolean;
-        private: boolean;
-        isTemplate: boolean;
-        position: number;
-        status: string;
-        projectId: string;
-        projectName: string;
-        DLM: null;
-      }
-    ]
+    tasklists: TaskListsListResponseItem[];
   }
 
 
   export interface TaskListsFetchResponse extends StatusResponse {
-    "todo-list": {
-      projectid: string;
-      name: string;
-      description: string;
-      "milestone-id": string;
-      "uncompleted-count": string;
-      complete: boolean;
-      private: string;
-      "overdue-count": string;
-      "project-name": string;
-      pinned: boolean;
-      id: string;
-      position: string;
-      "completed-count": string;
-    };
+    "todo-list": TaskListsFetchResponseItem;
   }
 
 
